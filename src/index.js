bb50f9646473d95e496b78fa28a605d4cb2a98d2
Lucas Magno <lmoabreu@gmail.com>
2021-07-17 15:19:58 -0300

    middleware p/ monitorar requisições

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/disciplinas', (request, response) => {
+function monitorarRequisicoes(request, response, next) {
+    const { method, url, params, body, query } = request;
+    const texto = `[${method} - ${url} - params: ${JSON.stringify(params)} - body ${JSON.stringify(body)} - query = ${JSON.stringify(query)}}]`;
+    console.log(texto);
+    return next();
+}
+
+// Para usar em todas requisições
+// app.use(monitorarRequisicoes);
+
+// Para usar em um grupo de requisições
+app.use('/disciplinas', monitorarRequisicoes);
+
+// Para usar em uma requisição específica
+// app.get('/disciplinas', monitorarRequisicoes, (request, response) => {
+// });
+
+app.get('/disciplinas', monitorarRequisicoes, (request, response) => {
     const query = request.query;
     return response.json(query);
 })
